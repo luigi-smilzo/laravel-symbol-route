0f1fd0b8ab4f5092d78be96d7d6fdb218c525ae3
Luigi <luigismilzo@live.com>
2020-06-10 17:28:36 +0200

    Main and footer style

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.main')
 
 @section('content')
-    <h3>About us</h3>
-    <p>Larafake is a super cool company and you should love us.</p>
-    <h4>Our stakeholders:</h4>
-    <ul>
-        
-            @foreach($stakeholders as $stakeholder)
-                <li>
-                    @if($loop->first)
-                        {{ $stakeholder." - CEO" }};
-                    @else
-                        {{ $stakeholder }};
-                    @endif
-                </li>
-            @endforeach
-        
-    </ul>
+    <div id="about">
+        <h3>About us</h3>
+        <p>Larafake is a super cool company and you should love us.</p>
+        <h4>Our stakeholders:</h4>
+        <ul>
+            
+                @foreach($stakeholders as $stakeholder)
+                    <li>
+                        @if($loop->first)
+                            {{ $stakeholder." - CEO" }};
+                        @else
+                            {{ $stakeholder }};
+                        @endif
+                    </li>
+                @endforeach
+            
+        </ul>
+    </div>
 @endsection
